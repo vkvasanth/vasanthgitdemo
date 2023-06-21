26bda6be52ba62a43a06d4f5128cf61540753529
vasanthkumar <vsnth2707@gmail.com>
2023-06-21 00:08:20 -0700

    i add a program

---
+++ sample.java
@@ -0,0 +1,40 @@
+public class sample{
+public static void main (String args[])
+{
+int a=19;
+int b=6;
+System.out.println(a+b);
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
